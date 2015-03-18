aa4b1a3c68097549f221f02fdd2c963a61d9d734
Al- Imran Ahmed <imran@polluxlab.com>
2015-03-18 13:26:51 +0600

    Lecture 13: Passing value to view form contorler and using url helper

--- 2015_03_17/ci/application/controllers/User.php
+++ 2015_03_17/ci/application/controllers/User.php
@@ -1,9 +1,13 @@
-<?php
-class User extends CI_Controller{
-	public function index(){
-		echo "This is the index of User";
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function index()
+	{
+		
 	}
-	public function show(){
-		echo "This is show page of User";
-	}
+
 }
+
+/* End of file User.php */
+/* Location: ./application/controllers/User.php */
