5ea5ed68ca1e3eb679e01e55fb20d5fdf9b8be45
Al- Imran Ahmed <imran@poluxlab.com>
2015-03-23 16:58:12 +0600

    db changed

--- ci_course.sql
+++ ci_course.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 22, 2015 at 09:07 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Mar 23, 2015 at 04:57 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,6 +23,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `department`
+--
+
+CREATE TABLE IF NOT EXISTS `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_estonian_ci NOT NULL,
+  `head` varchar(255) COLLATE utf8_estonian_ci NOT NULL,
+  `faculty` varchar(255) COLLATE utf8_estonian_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_estonian_ci AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `name`, `head`, `faculty`) VALUES
+(1, 'CSE', 'Akhter Hossain', 'FS&IT'),
+(2, 'EEE', 'Abu Taher', 'FS&IT'),
+(3, 'SWE', 'Tawhidul Islam', 'FS&IT'),
+(4, 'MTCA', 'Kabirul Islam', 'FS&IT');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -32,16 +56,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(64) DEFAULT NULL,
   `username` varchar(255) NOT NULL,
+  `department` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  UNIQUE KEY `email` (`email`),
+  KEY `department` (`department`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `username`) VALUES
-(10, 'Shoikot Iqbal', 'shoikot@yahoo.com', 'secret', 'shoikot');
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `username`, `department`) VALUES
+(10, 'Shoikot Iqbal', 'shoikot@yahoo.com', 'secret', 'shoikot', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
