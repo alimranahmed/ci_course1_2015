52a0f4e5c330e656b70fd4928dea8e99471a2a3d
rokibboss <rokiboss@gmail.com>
2015-04-26 22:25:33 +0600

    Htaccess Upload

--- ci_blog/application/views/backend/header.php
+++ ci_blog/application/views/backend/header.php
@@ -82,7 +82,7 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="<?php echo base_url('Dashboard'); ?>"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li class="active">
                         <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Comment</a>
