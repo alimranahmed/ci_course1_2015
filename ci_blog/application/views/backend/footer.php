8dcf4fa9ac567c6a37e10d806bb030891357f790
Al- Imran Ahmed <imran@polluxlab.com>
2015-04-26 13:07:49 +0600

    View sliced and created

---
+++ ci_blog/application/views/backend/footer.php
@@ -0,0 +1,18 @@
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url('public/js/jquery.js');?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url('public/js/bootstrap.min.js'); ?>"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="<?php echo base_url('public/js/plugins/morris/raphael.min.js'); ?>"></script>
+    <script src="<?php echo base_url('public/js/plugins/morris/morris.min.js');?>"></script>
+    <script src="<?php echo base_url('public/js/plugins/morris/morris-data.js');?>"></script>
+
+</body>
+
+</html>
---
+++ ci_blog/application/views/backend/v_create_post.php
@@ -0,0 +1,41 @@
+<div id="page-wrapper">
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Create Post
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i>  <a href="index.html">Post</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Create
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="heading" placeholder="Post Title">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" rows="10" name="body" placeholder="Write you post here..."></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="save" value="Create" class="btn btn-info">
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
