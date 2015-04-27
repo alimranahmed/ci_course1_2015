86d6b4eb9d2c396f4d4b064d2f9ec64159483b67
Al- Imran Ahmed <imran@polluxlab.com>
2015-04-27 13:16:48 +0600

    Category and tags added in create post form

--- ci_blog/application/views/backend/v_create_post.php
+++ ci_blog/application/views/backend/v_create_post.php
@@ -4,6 +4,7 @@
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
+                <h5 class="text-success"><?php echo $this->session->flashdata("successMsg"); ?></h5>
                 <h1 class="page-header">
                     Create Post
                 </h1>
@@ -20,12 +21,22 @@
         <!-- /.row -->
         <div class="row">
             <div class="col-lg-12">
-                <form>
+                <form method="post" action="<?php echo site_url('post/create')?>">
                     <div class="form-group">
                         <input type="text" class="form-control" name="heading" placeholder="Post Title">
                     </div>
                     <div class="form-group">
                         <textarea class="form-control" rows="10" name="body" placeholder="Write you post here..."></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="tags" placeholder="Tags, e.g. PHP,OOP,Codeigniter">
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control">
+                            <option value="">---Selct Category---</option>
+                            <option>PHP</option>
+                            <option>OOP</option>
+                        </select>
                     </div>
                     <div class="form-group">
                         <input type="submit" name="save" value="Create" class="btn btn-info">
