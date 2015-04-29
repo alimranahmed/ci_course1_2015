becce2451931756f0b88c11cb7796e59b6989fc4
shoikotmahmud <ashikmahmud62@gmail.com>
2015-04-29 13:24:10 +0600

    without login can not access dashboard

--- ci_blog/application/controllers/Dashboard.php
+++ ci_blog/application/controllers/Dashboard.php
@@ -1,8 +1,17 @@
 <?php
 class Dashboard extends CI_Controller{
 	public function index(){
-		$this->load->view("backend/header");
-		$this->load->view("backend/index");
-		$this->load->view("backend/footer");
+		if($this->session->userdata("user_id")){
+
+			$this->load->view("backend/header");
+			$this->load->view("backend/index");
+			$this->load->view("backend/footer");
+		}
+		else{
+			redirect(site_url('login'));
+		}
+
+		
 	}
+
 }
