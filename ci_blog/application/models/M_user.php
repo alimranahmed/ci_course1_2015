cc1f89a2d5843818c2550bfd8d04aedfc68c7f7a
rokibboss <rokiboss@gmail.com>
2015-04-29 10:40:09 +0600

    M_user in model is addad

---
+++ ci_blog/application/models/M_user.php
@@ -0,0 +1,11 @@
+<?php
+class M_user extends CI_Model{	
+	
+	public function checkCredintial($username, $password){
+		$this->db->select();
+		$this->db->from("users");
+		$this->db->where("username", $username);
+		$this->db->where("password", $password);
+		return $this->db->get()->row();
+	}
+}	
