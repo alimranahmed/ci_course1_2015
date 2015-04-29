c15b4d58f13846a9831349a20e603d6e28345a6e
rokibboss <rokiboss@gmail.com>
2015-04-29 10:37:32 +0600

    Login controller modified

--- ci_blog/application/controllers/Login.php
+++ ci_blog/application/controllers/Login.php
@@ -1,7 +1,29 @@
 <?php
 class Login extends CI_Controller{
 	public function index(){
-		$this->load->view("v_login");
+		$loginError = null;
+		if(isset($_POST["submit"])){
+			$username = $this->input->post("username");
+			$password = $this->input->post("password");
+			$this->load->model("M_user");
+			$user = $this->M_user->checkCredintial($username, $password);
+			if($user != null){
+				$this->session->set_userdata("user_id", $user->id);
+				$this->session->set_userdata("name", $user->name);
+				redirect(site_url('dashboard'));
+				//echo "You are a valid user";
+			}else{
+				$loginError = "Invalid username or password";
+			}
+			//var_dump($user);
+		}
+		$data = array(
+			"title" => "Login",
+			"loginError" => $loginError,
+			);
+		
+		$this->load->view("v_login", $data);
 		
 	}
+
 }
