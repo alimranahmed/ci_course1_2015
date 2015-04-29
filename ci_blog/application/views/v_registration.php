766529b47d3d460d1c98e9bdcc21111028f01b06
rokib <rokibboss@gmail.com>
2015-04-29 23:12:35 +0600

    New registration form added

--- ci_blog/application/views/v_registration.php
+++ ci_blog/application/views/v_registration.php
@@ -1,76 +1,132 @@
 <!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
 <html lang="en">
-
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>CI-Admin</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="<?php echo base_url('public/css/bootstrap.min.css'); ?>" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="<?php echo base_url('public/css/sb-admin.css'); ?>" rel="stylesheet">
-
-    <!-- Morris Charts CSS -->
-    <link href="<?php echo base_url('public/css/plugins/morris.css'); ?>" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="<?php echo base_url('public/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+<meta charset="utf-8"/>
+<title>Login </title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="" name="description"/>
+<meta content="" name="author"/>
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/global/plugins/font-awesome/css/font-awesome.min.css');?>" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/global/plugins/simple-line-icons/simple-line-icons.min.css');?>" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/global/plugins/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/global/plugins/uniform/css/uniform.default.css');?>" rel="stylesheet" type="text/css"/>
+<!-- END GLOBAL MANDATORY STYLES -->
+<!-- BEGIN PAGE LEVEL STYLES -->
+<link href="<?php echo base_url('assets/admin/pages/css/login.css');?>" rel="stylesheet" type="text/css"/>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME STYLES -->
+<link href="<?php echo base_url('assets/global/css/components.css');?>" id="style_components" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/global/css/plugins.css');?>" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/admin/layout/css/layout.css');?>" rel="stylesheet" type="text/css"/>
+<link href="<?php echo base_url('assets/admin/layout/css/themes/darkblue.css');?>" rel="stylesheet" type="text/css" id="style_color"/>
+<link href="<?php echo base_url('assets/admin/layout/css/custom.css');?>" rel="stylesheet" type="text/css"/>
+<!-- END THEME STYLES -->
+<link rel="shortcut icon" href="favicon.ico"/>
 </head>
-<body>
-	<div class="container">
-		<form method="post" action="<?php echo site_url('registration')?>" class="form" enctype="multipart/form-data">
-			  <div class="form-group">
-			    <input type="text" class="form-control" id="name" name="name" value="" placeholder="Enter your name">
-			  </div>
-			  <div class="form-group">
-			    <input type="email" class="form-control" id="email" name="email" value="" placeholder="Enter email">
-			  </div>
-			  <div class="form-group">
-			    <input type="text" class="form-control" id="username" name="username" value="" placeholder="Enter username">
-			  </div>
-			  <div class="form-group">
-			    <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Enter Password">
-			  </div>
-			  <div class="form-group">
-			    <input type="password" class="form-control" id="exampleInputPassword1" name="passconf" placeholder="Confirmation Password">
-			  </div>
-			   
-
-			  <div>
-			    <button type="submit" class="btn btn-primary" name="submit">Registration</button>
-			  </div>
-			</form>
+<!-- END HEAD -->
+<!-- BEGIN BODY -->
+<body class="login">
+<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+<div class="menu-toggler sidebar-toggler">
+</div>
+<!-- END SIDEBAR TOGGLER BUTTON -->
+<!-- BEGIN LOGO -->
+<div class="logo">
+	<a href="">
+	<h2>My Blog</h2>
+	</a>
+</div>
+<!-- END LOGO -->
+	
+	<!-- BEGIN REGISTRATION FORM -->
+<div class="content">	
+	<form class="" action="<?php echo site_url('registration')?>" method="post">
+		<h3>Sign Up</h3>
+		<p class="hint">
+			 Enter your personal details below:
+		</p>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Full Name</label>
+			<input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="name"/>
 		</div>
-	</div>
+		<div class="form-group">
+			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+			<label class="control-label visible-ie8 visible-ie9">Email</label>
+			<input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Username</label>
+			<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Password</label>
+			<input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password"/>
+		</div>
+		<div class="form-group">
+			<label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
+			<input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="passconf"/>
+		</div>
+		<div class="form-group margin-top-20 margin-bottom-20">
+			<label class="check">
+			<input type="checkbox" name="tnc"/> I agree to the <a href="javascript:;">
+			Terms of Service </a>
+			& <a href="javascript:;">
+			Privacy Policy </a>
+			</label>
+			<div id="register_tnc_error">
+			</div>
+		</div>
+		<div class="form-actions">
+			<button type="button" id="register-back-btn" class="btn btn-default">Back</button>
+			<button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right" name="submit">Submit</button>
+		</div>
+	</form>
+	<!-- END REGISTRATION FORM -->
+</div>
+<div class="copyright">
+	  2015&copy; My blog .
+</div>
+<!-- END LOGIN -->
+<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
+<!-- BEGIN CORE PLUGINS -->
+<!--[if lt IE 9]>
+<script src="<?php echo base_url('assets/global/plugins/respond.min.js');?>"></script>
+<script src="<?php echo base_url('assets/global/plugins/excanvas.min.js');?>"></script> 
+<![endif]-->
+<script src="<?php echo base_url('assets/global/plugins/jquery.min.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/global/plugins/jquery-migrate.min.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/global/plugins/bootstrap/js/bootstrap.min.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/global/plugins/jquery.blockui.min.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/global/plugins/jquery.cokie.min.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/global/plugins/uniform/jquery.uniform.min.js');?>" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="<?php echo base_url('assets/global/plugins/jquery-validation/js/jquery.validate.min.js');?>" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="<?php echo base_url('assets/global/scripts/metronic.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/admin/layout/scripts/layout.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/admin/layout/scripts/demo.js');?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/admin/pages/scripts/login.js');?>" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<script>
+jQuery(document).ready(function() {     
+Metronic.init(); // init metronic core components
+Layout.init(); // init current layout
+Login.init();
+Demo.init();
+});
+</script>
+<!-- END JAVASCRIPTS -->
 </body>
-
-
-    <!-- jQuery -->
-    <script src="<?php echo base_url('public/js/jquery.js');?>"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url('public/js/bootstrap.min.js'); ?>"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="<?php echo base_url('public/js/plugins/morris/raphael.min.js'); ?>"></script>
-    <script src="<?php echo base_url('public/js/plugins/morris/morris.min.js');?>"></script>
-    <script src="<?php echo base_url('public/js/plugins/morris/morris-data.js');?>"></script>
-
-</body>
-
+<!-- END BODY -->
 </html>
