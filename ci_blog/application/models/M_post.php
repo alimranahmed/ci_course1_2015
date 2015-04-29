f4f7aba7b99db6ccf87ce506182ac150a69d7f66
rokib <rokibboss@gmail.com>
2015-04-29 17:47:09 +0600

    In M_post models edit, delete, viewpost quary added

--- ci_blog/application/models/M_post.php
+++ ci_blog/application/models/M_post.php
@@ -8,4 +8,19 @@ class M_post extends CI_Model
 		$this->db->insert("posts",$data);
 		return $this->db->insert_id();
 	}
+	public function getAllPost(){
+		$this->db->select();
+		$this->db->from("posts");
+		return $this->db->get()->result();
+	}
+	public function deletePost($id){
+		$this->db->where("id",$id);
+		$this->db->delete('posts');	
+		
+	}
+	public function editPost($data, $id){
+		$this->db->where("id", $id);
+		$this->db->update("posts", $data);
+
+	}
 }
