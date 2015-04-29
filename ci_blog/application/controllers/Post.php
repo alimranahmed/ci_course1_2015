b787c12f2b9fd0d57b59191c62400712cf670a89
shoikotmahmud <ashikmahmud62@gmail.com>
2015-04-29 13:43:48 +0600

    Post controller modified

--- ci_blog/application/controllers/Post.php
+++ ci_blog/application/controllers/Post.php
@@ -5,23 +5,28 @@ class Post extends CI_Controller{
 		$this->load->model("M_post");
 	}
 	public function create(){
-		if(isset($_POST['save'])){
-			$heading = $this->input->post("heading");
-			$body = $this->input->post("body");
-			$tags = $this->input->post("tags");
-			$category = $this->input->post("category");
-			$newPost = [
-				"heading" => $heading,
-				"body" => $body,
-				"category" => $category,
-				"tags" => $tags,
-				"updated_at" => date("Y:m:d H:i:s"),
-			];
-			$this->M_post->create($newPost);
-			$this->session->set_flashdata("successMsg", "Post created successfully!");
+		if($this->session->userdata("user_id")){
+			if(isset($_POST['save'])){
+				$heading = $this->input->post("heading");
+				$body = $this->input->post("body");
+				$tags = $this->input->post("tags");
+				$category = $this->input->post("category");
+				$newPost = [
+					"heading" => $heading,
+					"body" => $body,
+					"category" => $category,
+					"tags" => $tags,
+					"updated_at" => date("Y:m:d H:i:s"),
+				];
+				$this->M_post->create($newPost);
+				$this->session->set_flashdata("successMsg", "Post created successfully!");
+			}
+			$this->load->view("backend/header");
+			$this->load->view("backend/v_create_post");
+			$this->load->view("backend/footer");
 		}
-		$this->load->view("backend/header");
-		$this->load->view("backend/v_create_post");
-		$this->load->view("backend/footer");
+		else{
+			redirect(site_url('login'));
+		}
 	}
 }
