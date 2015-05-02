f56dfb5fee3ef4073033e3da052b0e456a045516
rokib <rokibboss@gmail.com>
2015-05-02 14:38:12 +0600

    #4: Category controller added

---
+++ ci_blog/application/controllers/Category.php
@@ -0,0 +1,75 @@
+<?php
+class Category extends CI_Controller{
+	function __construct() {
+		parent::__construct();
+		$this->load->model("M_category");
+	}
+	
+	public function create(){
+		$this->loginCheck();
+		
+			if(isset($_POST['add'])){
+				$cat_name = $this->input->post("cat_name");
+				
+				$newCat = [
+					"name" => $cat_name,
+					
+				];
+				$this->M_category->create($newCat);
+				$this->session->set_flashdata("successMsg", "Category created successfully!");
+			}
+			
+		redirect(site_url('category/view_category'));
+	}
+	
+	public function view_category(){
+		
+		$this->loginCheck();
+		
+		$categoriesList = $this->M_category->getAllCategories();
+
+		$data = array(
+				"title" => "Home",
+				"categories" => $categoriesList,
+			);
+			
+		$this->load->view("backend/header");
+		$this->load->view("backend/v_category", $data);
+		$this->load->view("backend/footer");
+		
+	}
+		
+	public function edit($id){
+		$this->loginCheck();
+			$fileError = null;
+			$fileName = null;
+		    
+		    echo $fileError;
+				
+				$cat_name = $this->input->post("cat_name");
+				$data = array(
+					"name" => $cat_name,
+					
+					);
+				$this->M_category->editCategory($data, $id);
+				
+				redirect(site_url('category/view_category'));
+		
+		
+		
+	}
+	public function delete($id){
+		$this->loginCheck();
+		
+		$this->M_category->deleteCategory($id);
+		
+		redirect(site_url('category/view_category'));
+		
+	}
+	public function loginCheck(){
+		if(!$this->session->userdata("user_id")){
+			$this->session->set_flashdata('errorMsg', 'Unauthorized access');
+			redirect(site_url(Login));
+		}
+	}
+}
