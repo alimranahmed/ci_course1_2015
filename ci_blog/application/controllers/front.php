e6daf9d3331e83572db5d97d47448d678426ab68
rokib <rokibboss@gmail.com>
2015-05-12 18:14:40 +0600

    #8: add functionality to view posts

--- ci_blog/application/controllers/front.php
+++ ci_blog/application/controllers/front.php
@@ -1,10 +1,22 @@
 <?php
 class front extends CI_Controller{
+	function __construct() {
+		parent::__construct();
+		$this->load->model("M_front");
+	}
 
 	public function index(){
+		$postList = $this->M_front->getPost();
 
+		$data = array(
+				"title" => "Home",
+				"posts" => $postList,
+
+			);
+		
+		
 		$this->load->view("frontend/v_header");
-		$this->load->view("frontend/v_home");
+		$this->load->view("frontend/v_home",$data);
 		$this->load->view("frontend/v_footer"); 
 	} 
 }
