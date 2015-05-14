36e61642c8a83fa45aba70182e504cd4b14ce8af
shoikotmahmud <ashikmahmud62@gmail.com>
2015-05-14 19:19:55 +0600

    #9: added function frontPost in M_front

--- ci_blog/application/models/M_front.php
+++ ci_blog/application/models/M_front.php
@@ -7,5 +7,13 @@ class M_front extends CI_Model{
 		$this->db->order_by('created_at', 'DESC');
 		return $this->db->get()->result();
 	}
+
+	public function frontPost($id){
+
+		$this->db->select();
+		$this->db->from("posts");
+		$this->db->where('id', $id);
+		return $this->db->get()->result();
+	}
 	
 }
