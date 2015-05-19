772a3c6d8dfda88d0122f86976104f3d375fe1e9
shoikotmahmud <ashikmahmud62@gmail.com>
2015-05-19 11:17:58 +0600

    #9:just add comment box in frontpost.php

--- ci_blog/application/views/frontend/frontpost.php
+++ ci_blog/application/views/frontend/frontpost.php
@@ -2,6 +2,11 @@
 
     <!-- Main Content -->
     <div class="container">
+    
+    <?php if(isset($loginError)): ?>
+        <div class="text-danger"><?php echo $loginError ?></div>
+    <?php endif ?>
+       
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             
@@ -27,11 +32,24 @@
                     <tr class="info">
                     
                      <td><?php echo $comment->body; ?></td>
-                        
+                     
+                                                
                     </tr>
                     <?php endforeach ?>   
 
 
+                </div>
+                <div>
+                    <form method='post'>
+                          
+                          Comment:<br>
+                          <textarea name='comment' id='comment'></textarea><br />
+
+
+                          <input type='hidden' name='articleid' id='articleid' value='<? echo $_GET["id"]; ?>' />
+
+                          <input type='submit' value='Submit' />  
+                    </form>
                 </div>
                 
                 <!-- Pager -->
